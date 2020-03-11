0b1d88655b4f757aa5efac7b64b7f159a3cce344
Caleb Schwind <caleb.schwind@erg.com>
2020-03-11 12:26:41 -0400

    #26 Narrow down string types in the Layer and Edits files.

--- src/types/Edits.ts
+++ src/types/Edits.ts
@@ -1,3 +1,5 @@
+import { AddedFrom, LayerTypeName } from 'types/Layer';
+
 export type EditsType = {
   count: number;
   edits: LayerEditsType[];
@@ -7,8 +9,8 @@ export type LayerEditsType = {
   id: number; // layer id
   layerId: string; // id from esri layer
   name: string; // layer name
-  layerType: string; // type of tots layer (sample, contamination, etc.)
-  addedFrom: string; // how the layer was added (file, url, etc.)
+  layerType: LayerTypeName; // type of tots layer (sample, contamination, etc.)
+  addedFrom: AddedFrom; // how the layer was added (file, url, etc.)
   adds: FeatureEditsType[]; // features to add
   updates: FeatureEditsType[]; // features to update
   deletes: FeatureEditsType[]; // features to delete
--- src/types/Layer.ts
+++ src/types/Layer.ts
@@ -1,13 +1,27 @@
+export type LayerTypeName =
+  | 'Contamination Map'
+  | 'Samples'
+  | 'Reference Layer'
+  | 'Area of Interest'
+  | 'VSP';
+
+export type LayerSelectType = {
+  value: LayerTypeName;
+  label: LayerTypeName;
+};
+
+export type AddedFrom = 'file' | 'sketch';
+
 export type LayerType = {
   id: number;
   layerId?: string;
   value: string;
   name: string;
   label: string;
-  layerType: string;
+  layerType: LayerTypeName;
   defaultVisibility: boolean;
   geometryType: string;
-  addedFrom: string;
+  addedFrom: AddedFrom;
   sketchLayer: __esri.GraphicsLayer | __esri.FeatureLayer;
 };
 
