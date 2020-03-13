1972e0229eaf38ac77406e1b71c5be784c63da82
Caleb Schwind <caleb.schwind@erg.com>
2020-03-13 16:14:43 -0400

    #19 Set GP Service to use https.

--- src/config/webService.ts
+++ src/config/webService.ts
@@ -1,4 +1,4 @@
 //TODO - Don't forget to change http back to https when the GPServer
 //  goes public.
 export const totsGPServer =
-  'http://4.36.57.16/arcgis/rest/services/TOTS/GPServer';
+  'https://4.36.57.16/arcgis/rest/services/TOTS/GPServer';
