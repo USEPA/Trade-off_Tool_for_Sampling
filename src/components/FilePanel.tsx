f5ca3e5f59cc5a990b354473c0abb86fb0d179fd
Caleb Schwind <caleb.schwind@erg.com>
2020-04-30 15:27:19 -0400

    Created some tests for the uploading different files on the file upload panel.

--- src/components/FilePanel.tsx
+++ src/components/FilePanel.tsx
@@ -1042,7 +1042,7 @@ function FilePanel() {
                   </label>
                   <br />
                   <div {...getRootProps({ className: 'dropzone' })}>
-                    <input {...getInputProps()} />
+                    <input data-testid="tots-dropzone" {...getInputProps()} />
                     {isDragActive ? (
                       <p>Drop the files here ...</p>
                     ) : (
