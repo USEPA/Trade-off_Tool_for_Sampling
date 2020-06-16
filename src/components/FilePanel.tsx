99c6b981786007443a35ac94ce0598af17ce00a4
Caleb Schwind <caleb.schwind@erg.com>
2020-06-16 11:42:43 -0400

    #53 Moved the file type explanation so that it is only visible prior to selecting a layer type.

--- src/components/FilePanel.tsx
+++ src/components/FilePanel.tsx
@@ -158,7 +158,7 @@ const layerInfo = css`
 `;
 
 const sectionParagraph = css`
-  padding-top: 1.5em;
+  padding-top: 1em;
   padding-bottom: 1em;
 `;
 
@@ -1023,7 +1023,29 @@ function FilePanel() {
         }}
         options={layerOptions}
       />
-      {layerType && (
+      {!layerType ? (
+        <React.Fragment>
+          <p css={sectionParagraph}>
+            <i className="fas fa-exclamation-triangle" css={warningIcon} />{' '}
+            Requirements for uploading files:
+          </p>
+          <p css={layerInfo}>
+            <strong>Shapefile</strong> - ZIP archive containing all required
+            shapefile files
+          </p>
+          <p css={layerInfo}>
+            <strong>CSV or TXT</strong> - files with optional address, place or
+            coordinate locations (comma, semi-colon or tab delimited)
+          </p>
+          <p css={layerInfo}>
+            <strong>GPX</strong> - GPS Exchange Format
+          </p>
+          <p css={layerInfo}>
+            <strong>GeoJSON</strong> - open standard format for simple
+            geographical
+          </p>
+        </React.Fragment>
+      ) : (
         <React.Fragment>
           {layerType.value === 'VSP' && (
             <React.Fragment>
@@ -1039,6 +1061,13 @@ function FilePanel() {
                 }}
                 options={SampleSelectOptions}
               />
+              <p css={sectionParagraph}>
+                A Visual Sample Plan (VSP) layer helps determine where sampling
+                should be conducted and how many samples are needed based on a
+                variety of situation-specific parameters. Once added, you can
+                select this layer in the next step, <strong>Create Plan</strong>
+                , and use it to create the Sampling Plan.
+              </p>
             </React.Fragment>
           )}
           {(layerType.value !== 'VSP' ||
@@ -1102,41 +1131,6 @@ function FilePanel() {
                       </p>
                     </React.Fragment>
                   )}
-                  {layerType.value === 'VSP' && (
-                    <React.Fragment>
-                      <p css={sectionParagraph}>
-                        A Visual Sample Plan (VSP) layer helps determine where
-                        sampling should be conducted and how many samples are
-                        needed based on a variety of situation-specific
-                        parameters. Once added, you can select this layer in the
-                        next step, <strong>Create Plan</strong>, and use it to
-                        create the Sampling Plan.
-                      </p>
-                    </React.Fragment>
-                  )}
-                  <p css={sectionParagraph}>
-                    <i
-                      className="fas fa-exclamation-triangle"
-                      css={warningIcon}
-                    />{' '}
-                    Requirements for uploading files:
-                  </p>
-                  <p css={layerInfo}>
-                    <strong>Shapefile</strong> - ZIP archive containing all
-                    required shapefile files
-                  </p>
-                  <p css={layerInfo}>
-                    <strong>CSV or TXT</strong> - files with optional address,
-                    place or coordinate locations (comma, semi-colon or tab
-                    delimited)
-                  </p>
-                  <p css={layerInfo}>
-                    <strong>GPX</strong> - GPS Exchange Format
-                  </p>
-                  <p css={layerInfo}>
-                    <strong>GeoJSON</strong> - open standard format for simple
-                    geographical
-                  </p>
                   {layerType.value === 'Samples' &&
                     attributeOverwriteWarning(null)}
                   {layerType.value === 'VSP' &&
