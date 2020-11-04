a30a32fb64decd7d197a89185df9a0818610e778
Caleb Schwind <caleb.schwind@erg.com>
2020-11-04 14:36:04 -0500

    #327 Updated the "Save" button to be hidden until a change is made.

--- src/components/MapPopup.tsx
+++ src/components/MapPopup.tsx
@@ -241,38 +241,38 @@ function MapPopup({
                 />
               </div>
               <div css={saveButtonContainerStyles}>
-                <button
-                  css={saveButtonStyles(saveStatus)}
-                  disabled={
-                    graphicNote === note &&
-                    activeLayerId === selectedLayer?.layerId
-                  }
-                  onClick={(ev) => {
-                    // set the notes
-                    if (feature?.graphic) {
-                      feature.graphic.attributes['Notes'] = note;
-                      setGraphicNote(note);
+                {(graphicNote !== note ||
+                  activeLayerId !== selectedLayer?.layerId) && (
+                  <button
+                    css={saveButtonStyles(saveStatus)}
+                    onClick={(ev) => {
+                      // set the notes
+                      if (feature?.graphic) {
+                        feature.graphic.attributes['Notes'] = note;
+                        setGraphicNote(note);
 
-                      // move the graphic if it is on a different layer
-                      if (activeLayerId !== selectedLayer?.layerId) {
-                        onClick(ev, feature, 'Move', selectedLayer);
+                        // move the graphic if it is on a different layer
+                        if (activeLayerId !== selectedLayer?.layerId) {
+                          onClick(ev, feature, 'Move', selectedLayer);
+                        } else {
+                          onClick(ev, feature, 'Save');
+                        }
+
+                        setSaveStatus('success');
                       } else {
-                        onClick(ev, feature, 'Save');
+                        setSaveStatus('failure');
                       }
-
-                      setSaveStatus('success');
-                    } else {
-                      setSaveStatus('failure');
-                    }
-                  }}
-                >
-                  {saveStatus === 'none' && 'Save'}
-                  {saveStatus === 'failure' && (
-                    <React.Fragment>
-                      <i className="fas fa-exclamation-triangle" /> Error
-                    </React.Fragment>
-                  )}
-                </button>
+                    }}
+                  >
+                    {saveStatus === 'failure' ? (
+                      <React.Fragment>
+                        <i className="fas fa-exclamation-triangle" /> Error
+                      </React.Fragment>
+                    ) : (
+                      'Save'
+                    )}
+                  </button>
+                )}
               </div>
             </React.Fragment>
           )}
