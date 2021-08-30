48c483d32c4f0386033393e3f81fd77b2daa784a
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2021-08-30 12:57:49 -0400

    #515 Fixed an issue with the legend not populating correctly.

--- app/client/src/components/Toolbar.tsx
+++ app/client/src/components/Toolbar.tsx
@@ -284,7 +284,7 @@ function Toolbar() {
     setLayerToRemove, //
   ] = React.useState<__esri.Layer | null>(null);
   React.useEffect(() => {
-    if (!mapView) return;
+    if (!mapView || layers.length === 0 || legendInitialized) return;
 
     // clear out the legend container
     const legendContainer: HTMLElement | null = document.getElementById(
@@ -545,7 +545,7 @@ function Toolbar() {
     });
 
     setLegendInitialized(true);
-  }, [Collection, LayerList, Legend, Slider, mapView, defaultSymbols]);
+  }, [Collection, LayerList, Legend, Slider, mapView, defaultSymbols, layers, legendInitialized]);
 
   // Deletes layers from the map and session variables when the delete button is clicked
   React.useEffect(() => {
@@ -754,7 +754,6 @@ function Toolbar() {
         <div>
           <button
             css={toolBarButtonStyles}
-            disabled={!legendInitialized}
             className={basemapVisible ? 'tots-button-selected' : ''}
             onClick={(ev) => {
               setBasemapVisible(!basemapVisible);
@@ -771,7 +770,6 @@ function Toolbar() {
         <div>
           <button
             css={toolBarButtonStyles}
-            disabled={!legendInitialized}
             className={legendVisible ? 'tots-button-selected' : ''}
             onClick={(ev) => {
               setLegendVisible(!legendVisible);
@@ -780,7 +778,11 @@ function Toolbar() {
           >
             Legend{' '}
           </button>
-          <div css={legendStyles(legendVisible)} id="legend-container" />
+          <div css={legendStyles(legendVisible)} id="legend-container">
+            <div className="esri-layer-list__no-items">
+              There are currently no items to display.
+            </div>
+          </div>
         </div>
         {oAuthInfo && (
           <button
