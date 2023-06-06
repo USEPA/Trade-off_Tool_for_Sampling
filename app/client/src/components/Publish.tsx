936b2bda3051f7020ef47d03562b1df50df2d186
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2023-06-06 17:09:34 -0400

    TOTS-53 Fixed issue of saving calculate settings to existing layer.

--- app/client/src/components/Publish.tsx
+++ app/client/src/components/Publish.tsx
@@ -979,7 +979,7 @@ function Publish() {
     let layerEdits: LayerEditsType = {
       type: 'layer',
       id: editsScenario.id,
-      pointsId: -1,
+      pointsId: editsScenario.pointsId,
       uuid: '', // no need for a uuid since this is combining layers into one
       layerId: editsScenario.layerId,
       portalId: editsScenario.portalId,
