a467bb89e0363b68c1c070c46b58da8bb0d9c331
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2023-06-29 10:14:45 -0400

    TOTS-54 Added a cypress test for the notes in file uploads.

--- app/cypress/e2e/file-panel.cy.ts
+++ app/cypress/e2e/file-panel.cy.ts
@@ -29,4 +29,36 @@ describe("File Panel", () => {
       cy.findByTestId("tots-dropzone").upload(file, "BOTE.zip");
     });
   });
+
+  it("Verify notes in file upload", () => {
+    cy.findByRole("button", { name: "OK" })
+      .should("exist")
+      .click({ force: true });
+    cy.findByRole("button", { name: "Add Data" })
+      .should("exist")
+      .click({ force: true });
+    cy.get("#add-data-select").type("Add Layer from File{enter}");
+    cy.get("#layer-type-select-input").type("Samples{enter}");
+    cy.fixture("targeted-sampling-with-notes.zip").then((file) => {
+      cy.findByTestId("tots-dropzone").upload(
+        file,
+        "targeted-sampling-with-notes.zip"
+      );
+    });
+
+    cy.findByText("Sample Issues");
+
+    cy.findByRole("button", { name: "Continue" })
+      .should("exist")
+      .click({ force: true });
+
+    cy.findByText("Upload Succeeded", { timeout: 120000 });
+
+    // Come up with a way to check if notes were added
+    cy.findByRole("button", { name: "Expand Table Panel" })
+      .should("exist")
+      .click();
+
+    cy.findByText("Note Sample 35", { exact: false });
+  });
 });
